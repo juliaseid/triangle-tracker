c145f558c74eba2e183e8a56a39684696543599a
Julia Seidman <juliaseid@gmail.com>
2020-03-26 16:09:26 -0700

    Finally make everything work and show up

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,55 @@
 
-//Business Logic:
-//Function checks if it's a triangle at all: If (side1 + side2)<=side3 || (side2 + side3)<=side1, NaT
-var side1 = parseInt($("#side1").val());
-var side2 = parseInt($("#side2").val());
-var side3 = parseInt($("#side3").val());
-
 function NaT(side1, side2, side3){
-  var NaT = Boolean;
-  if ((side1 + side2)<=side3), NaT=true;
-  else if ((side2 + side3)<=side1), NaT=true;
-  else
+  var NaT = false;
+  if ((side1 + side2)<=side3) {
+     NaT=true;
+  }
+  else if ((side2 + side3)<=side1){
+    NaT=true;
+  } 
+  else {
+    NaT=false;
+  }
+  return NaT;
+};
 
-}
+function sidesSame(s, t) {
+  if (s===t) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
 
-//If NaT===False, Function checks equivalency pairs (1+2, 2+3, 1+3), returns booleans
-//If  side1=side2 True || side2=side3 || side1=side3 True, but (side2!=side1 || side2 != side3), isoceles;
-//Else If side 1=side2 True && side2=side3 True, equilateral;
-//Else, scalene;
 
-
-
-
-//User Interface:
-//If isoceles, "You made an isoceles triangle! Is it wide and flat or tall and narrow?"
-//If equilateral, "You made an equilateral triangle!  So even!"
-//If scalene, "You made a scalene triangle!  Way to think different!"
-//If NaT, "That won't make a triangle, because the sides can't reach each other!"
 
 $(document).ready(function() {
-  $("form#triangleFun").submit(function() {
-    if 
+  $("#triangleFun").submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+
+    if (NaT(side1, side2, side3)===true) {
+      var text = "That won't make a triangle, because the sides can't reach each other!"
+    }
+    
+    else if ((sidesSame(side1, side2)===true) && (sidesSame(side2, side3)===true) && (sidesSame(side1, side3)===true)) {
+      var text = "You made an equilateral triangle!  So even!"
+    }
+
+    else if ((sidesSame(side1, side2)===true) || (sidesSame(side2, side3)===true) || (sidesSame(side1, side3)===true)) {
+      var text = "You made an isoceles triangle! Is it wide and flat or tall and narrow?"
+    }
+
+    else {
+      var text = "You made a scalene triangle!  Way to think different!"
+    }
+
+    $("#output").empty().append(text);
+    $("#output").show();
+  
+    event.preventDefault();
+  
+  });
+});
